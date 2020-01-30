e8b60d53eb090c87ceb273aa36d7765cd0c1506b
Goraved <roman.pobotin@gmail.com>
2020-01-30 14:11:18 +0200

    Add error handling and pages

--- app.py
+++ app.py
@@ -77,5 +77,15 @@ def cancel_endpoint(rehearsal_id):
     return str(rehearsals).replace("'", '"')
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html', title='404'), 404
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', title='500'), 500
+
+
 if __name__ == "__main__":
     app.run()
